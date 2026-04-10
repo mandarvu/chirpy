53ae10300ab5075da93da4986ff55206ee91e74a
Mandar Upasani <mandar1695@gmail.com>
2026-04-10 22:18:15 +0530

    Adding a simple server handler that just returns 404 everytime

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		http.NotFound(w, req)
+	})
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
