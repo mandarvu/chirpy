8964a62c2d5fad86133f8edb519f0125577a5096
Mandar Upasani <mandar1695@gmail.com>
2026-04-11 09:19:54 +0530

    Adding a handler to get status of server
    Moving root to app/

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+
+	mux.Handle("/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("./app/assets"))))
+
+	statusHandler := func(r http.ResponseWriter, req *http.Request) {
+		r.Header().Add("Content-type", "text/plain; charset=utf-8")
+		r.WriteHeader(200)
+		r.Write([]byte("OK"))
+	}
+
+	mux.HandleFunc("/healthz", statusHandler)
 
 	server := http.Server{
 		Handler: mux,
