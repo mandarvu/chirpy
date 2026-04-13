2eac0a0bd3e6cd749ea77c9439e6237b5afcc9f3
Mandar Upasani <mandar1695@gmail.com>
2026-04-13 20:54:19 +0530

    Adding a visit counter as a wrapper on main handlers

--- main.go
+++ main.go
@@ -2,14 +2,34 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(r http.ResponseWriter, req *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(r, req)
+	})
+}
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))))
+	conf := apiConfig{
+		fileServerHits: atomic.Int32{},
+	}
 
-	mux.Handle("/assets/", http.StripPrefix("/app", http.FileServer(http.Dir("./app/assets"))))
+	dirHandler := func(dir string) http.Handler {
+		return http.StripPrefix("/"+dir, http.FileServer(http.Dir("./"+dir)))
+	}
+
+	mux.Handle("/app/", conf.middlewareMetricsInc(dirHandler("app")))
+
+	mux.Handle("/assets/", dirHandler("assets"))
 
 	statusHandler := func(r http.ResponseWriter, req *http.Request) {
 		r.Header().Add("Content-type", "text/plain; charset=utf-8")
