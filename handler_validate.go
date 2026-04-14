1c98d20f845a986a1fc1804d303b1d27ff3f8c68
Mandar Upasani <mandar1695@gmail.com>
2026-04-14 18:02:06 +0530

    cleaning profane words

--- handler_validate.go
+++ handler_validate.go
@@ -3,14 +3,23 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	profane := map[string]string{
+		"kerfuffle": "",
+		"sharbert":  "",
+		"fornax":    "",
+	}
+
 	type parameters struct {
 		Body string `json:"body"`
 	}
+
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		Valid bool   `json:"valid"`
+		Body  string `json:"cleaned_body"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -27,7 +36,17 @@ func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	cleanedBody := []string{}
+
+	for word := range strings.SplitSeq(params.Body, " ") {
+		if _, ok := profane[strings.ToLower(word)]; ok {
+			word = "****"
+		}
+		cleanedBody = append(cleanedBody, word)
+	}
+
 	respondWithJSON(w, http.StatusOK, returnVals{
 		Valid: true,
+		Body:  strings.Join(cleanedBody, " "),
 	})
 }
