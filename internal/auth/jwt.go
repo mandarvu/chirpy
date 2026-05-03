7f7d41d8814394522787b9b1e7e33a61740a1e5a
Mandar Upasani <mandar1695@gmail.com>
2026-05-03 17:56:28 +0530

    JWT creation and validation
    Test cases added

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -3,7 +3,48 @@ package auth
 import (
 	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/google/uuid"
 )
 
-func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error)
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy-access",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	outputJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	signedToken, err := outputJWT.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (any, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, jwt.ErrSignatureInvalid
+		}
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	id, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	userID, err := uuid.Parse(id)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+
+	return userID, nil
+}
---
+++ internal/auth/jwt_test.go
@@ -0,0 +1,65 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestMakeJWT(t *testing.T) {
+	tests := []struct {
+		name        string
+		userID      uuid.UUID
+		tokenSecret string
+		expiresIn   time.Duration
+		expectError bool
+	}{
+		{
+			name:        "normal token",
+			userID:      uuid.New(),
+			tokenSecret: "mysecret",
+			expiresIn:   time.Second * 1,
+			expectError: false,
+		},
+		{
+			name:        "empty token secret",
+			userID:      uuid.New(),
+			tokenSecret: "",
+			expiresIn:   time.Second * 1,
+			expectError: false,
+		},
+		{
+			name:        "expired token",
+			userID:      uuid.New(),
+			tokenSecret: "mysecret",
+			expiresIn:   -time.Hour,
+			expectError: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			jwt, err := MakeJWT(test.userID, test.tokenSecret, test.expiresIn)
+			if err != nil {
+				t.Fatalf("failed to create JWT: %v", err)
+			}
+
+			token, err := ValidateJWT(jwt, test.tokenSecret)
+			if test.expectError {
+				if err == nil {
+					t.Fatal("expected error but got none")
+				}
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("failed to validate JWT: %v", err)
+			}
+
+			if token != test.userID {
+				t.Errorf("expected userID %v, got %v", test.userID, token)
+			}
+		})
+	}
+}
